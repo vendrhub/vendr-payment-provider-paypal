ce58eadffd47f59d118fe32c1e508adf18a10b3a
mattbrailsford <me@mattbrailsford.com>
2021-09-30 10:03:24 +0100

    Initial conversion to Vendr v2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Vendr PayPal Payment Provider 
 
-PayPal Payment Provider for Vendr, the eCommerce package for Umbrao v8+
+PayPal Payment Provider for Vendr, the eCommerce package for Umbraco
---
+++ src/Vendr.PaymentProvider.PayPal/PayPalSettingsBase.cs
@@ -0,0 +1,65 @@
+﻿using Vendr.Core.PaymentProviders;
+
+namespace Vendr.PaymentProviders.PayPal
+{
+    public class PayPalSettingsBase
+    {
+        [PaymentProviderSetting(Name = "Continue URL", 
+            Description = "The URL to continue to after this provider has done processing. eg: /continue/",
+            SortOrder = 100)]
+        public string ContinueUrl { get; set; }
+
+        [PaymentProviderSetting(Name = "Cancel URL", 
+            Description = "The URL to return to if the payment attempt is canceled. eg: /cancel/",
+            SortOrder = 200)]
+        public string CancelUrl { get; set; }
+
+        [PaymentProviderSetting(Name = "Error URL",
+            Description = "The URL to return to if the payment attempt errors. eg: /error/",
+            SortOrder = 300)]
+        public string ErrorUrl { get; set; }
+
+        [PaymentProviderSetting(Name = "Sandbox Client ID",
+            Description = "Your sandbox PayPal client id",
+            SortOrder = 400)]
+        public string SandboxClientId { get; set; }
+
+        [PaymentProviderSetting(Name = "Sandbox Secret",
+            Description = "Your sandbox PayPal secret",
+            SortOrder = 500)]
+        public string SandboxSecret { get; set; }
+
+        [PaymentProviderSetting(Name = "Sandbox Webhook ID",
+            Description = "Your sandbox PayPal webhook id",
+            SortOrder = 600)]
+        public string SandboxWebhookId { get; set; }
+
+        [PaymentProviderSetting(Name = "Live Client ID",
+            Description = "Your live PayPal client id",
+            SortOrder = 700)]
+        public string LiveClientId { get; set; }
+
+        [PaymentProviderSetting(Name = "Live Secret",
+            Description = "Your live PayPal secret",
+            SortOrder = 800)]
+        public string LiveSecret { get; set; }
+
+        [PaymentProviderSetting(Name = "Live Webhook ID",
+            Description = "Your live PayPal webhook id",
+            SortOrder = 900)]
+        public string LiveWebhookId { get; set; }
+
+        [PaymentProviderSetting(Name = "Sandbox Mode",
+            Description = "Set whether to process payments in sandbox mode.",
+            SortOrder = 1000000)]
+        public bool SandboxMode { get; set; }
+
+        // Advanced settings
+        [PaymentProviderSetting(Name = "Brand Name",
+            Description = "A brand name to override the business name with on the PayPal Checkout pages ",
+            SortOrder = 100,
+            IsAdvanced = true)]
+        public string BrandName { get; set; }
+
+    }
+}
